c88a55fd452d80eee2dab05e1ae67d0b31c9278f
Raj <rajpansuriya40@gmail.com>
2022-04-01 16:12:12 +0530

    feat: add tempalates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts=[
+    {
+        'author':'Jack Racher',
+        'title':'Blog post 1',
+        'content':'First post content',
+        'date_posted':'March 31st 2022'
+    },
+    {
+        'author':'Jack Ryan',
+        'title':'Blog post 2',
+        'content':'Second post content',
+        'date_posted':'April 1st 2022'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Blog About<h1>")
+    return render(request, 'blog/about.html', {'title':'About'})
